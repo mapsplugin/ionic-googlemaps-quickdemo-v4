729e91688e8a211e34a707691fccd08b876b2d9e
wf9a5m75 <wf9a5m75@gmail.com>
2018-08-20 18:25:30 -0700

    update

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,7 +4,7 @@ import { Routes, RouterModule } from '@angular/router';
 const routes: Routes = [
   {
     path: '',
-    redirectTo: 'marker-cluster',
+    redirectTo: 'home',
     pathMatch: 'full'
   },
   {
