6f0616c5718a952b2859507c513afa862c6050c9
wf9a5m75 <wf9a5m75@gmail.com>
2018-09-18 13:30:23 -0700

    save

---
+++ bin/local_dev.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+GMAPS_DIR_PATH=$1
+if [ -z "${GMAPS_DIR_PATH}" ]; then
+  echo "Usage:"
+  echo " $> npm run link (path_to_@ionic-native/google-maps directory)"
+  exit 0
+fi
+
+CHECK=`ls -l ${GMAPS_DIR_PATH}/package.json 2>&1`
+
+if [[ $CHECK = *"No such file or directory"* ]]; then
+  (>&2 echo "There is no package.json at ${GMAPS_DIR_PATH}")
+else
+  rm -rf node_modules/\@ionic-native/plugins/google-maps
+
+  cp -R ${GMAPS_DIR_PATH}/dist/\@ionic-native/core node_modules/\@ionic-native/
+  npm link ${GMAPS_DIR_PATH}/dist/\@ionic-native/plugins/google-maps
+fi
