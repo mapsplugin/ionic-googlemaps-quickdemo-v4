f3bab555004a68136d87affb4c9853d8a14dab04
Masashi Katsumata <wf9a5m75@gmail.com>
2020-03-30 23:22:14 -0700

    Remove the batch geocoding example.

--- src/app/geocoding/geocoding.page.ts
+++ src/app/geocoding/geocoding.page.ts
@@ -28,7 +28,6 @@ export class GeocodingPage implements OnInit {
     // you have to wait the event.
     await this.platform.ready();
     await this.loadMap1();
-    await this.loadMap2();
   }
 
   loadMap1() {
@@ -70,59 +69,4 @@ export class GeocodingPage implements OnInit {
   }
 
 
-  loadMap2() {
-    this.map2 = GoogleMaps.create('map_canvas2', {
-      camera: {
-        target: [
-          {"lat": 37.789, "lng": -122.38},
-          {"lat": 32.909, "lng": -117.181}
-        ]
-      }
-    });
-  }
-  async onButton2_click(event) {
-    this.map2.clear();
-
-    this.loading = await this.loadingCtrl.create({
-      message: 'Please wait...'
-    });
-    await this.loading.present();
-    let start = Date.now();
-
-    // Geocode multiple location
-    Geocoder.geocode({
-
-      // Google office locations in California, USA
-      "address": [
-        "19510 Jamboree Road, Irvine, CA 92612, United States",
-        "803 11th Avenue, Sunnyvale, CA 94089, United States",
-        "6420 Sequence Dr, Suite 400, San Diego, CA 92121, United States",
-        "345 Spear Street, San Francisco, CA 94105, United States",
-        "901 Cherry Avenue, San Bruno, CA 94066, United States",
-        "12422 W. Bluff Creek Drive,Playa Vista, CA 90094,United States",
-        "1600 Amphitheatre Parkway,Mountain View, CA 94043,United States",
-        "340 Main Street,Los Angeles, CA 90291,United States",
-      ]
-    })
-    .then((mvcArray: BaseArrayClass<GeocoderResult[]>) => {
-
-      mvcArray.on('insert_at').subscribe((params: any[]) => {
-        const index: number = params[0];
-        const result: GeocoderResult = mvcArray.getAt(index);
-        this.map2.addMarkerSync({
-          'position': result[0].position,
-          'title':  JSON.stringify(result)
-        });
-      });
-      mvcArray.one('finish').then(() => {
-        this.loading.dismiss();
-        let end = Date.now();
-        alert("duration: " + ((end - start) / 1000).toFixed(1) + " seconds");
-
-        let results: any[] = mvcArray.getArray();
-        console.log(results);
-      });
-
-    });
-  }
 }
